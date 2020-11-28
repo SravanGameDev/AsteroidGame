fea227c6f3b606d9580a6120931b0b9de2766267
SRAVAN KUMAR K <sravan.kumar1691@gmail.com>
2020-11-28 20:08:21 +0530

    Revert "Added Score text but It is enabled in a different Sate"
    This reverts commit 770d85f9f6538583a32eb6b3322afcb6aba3db88.

--- Asteroids/LevelStart.cpp
+++ Asteroids/LevelStart.cpp
@@ -6,8 +6,7 @@
 
 LevelStart::LevelStart() :
 	level_(0),
-	delay_(0),
-	score_(0)
+	delay_(0)
 {
 }
 
@@ -15,8 +14,6 @@ void LevelStart::OnActivate(System *system, StateArgumentMap &args)
 {
 	level_ = args["Level"].asInt;
 	delay_ = 120;
-	score_ = args["Score"].asInt;
-
 }
 
 void LevelStart::OnUpdate(System *system)
@@ -25,7 +22,6 @@ void LevelStart::OnUpdate(System *system)
 	{
 		GameState::StateArgumentMap args;
 		args["Level"].asInt = level_;
-		args["Score"].asInt = score_;
 		system->SetNextState("PlayingState", args);
 	}
 }
@@ -38,20 +34,11 @@ void LevelStart::OnRender(System *system)
 	system->GetGame()->RenderBackgroundOnly(graphics);
 
 	char levelStartText[256];
-	sprintf_s(levelStartText, "Level %d", level_);
+	sprintf(levelStartText, "Level %d", level_);
 	int textWidth = fontEngine->CalculateTextWidth(levelStartText, FontEngine::FONT_TYPE_LARGE);
 	int textX = (800 - textWidth) / 2;
 	int textY = (600 - 48) / 2;
 	fontEngine->DrawText(levelStartText, textX, textY, 0xff00ffff, FontEngine::FONT_TYPE_LARGE);
-
-
-	char scoreText[256];
-	sprintf_s(scoreText, "Score: %d", score_);
-	int scoreWidth = fontEngine->CalculateTextWidth(scoreText, FontEngine::FONT_TYPE_SMALL);
-	int scoreX = (800 - scoreWidth) / 20;
-	int scoreY = (600 - 48) / 20;
-	fontEngine->DrawText(scoreText, scoreX, scoreY, 0xff00ffff, FontEngine::FONT_TYPE_SMALL);
-
 }
 
 void LevelStart::OnDeactivate(System *system)
